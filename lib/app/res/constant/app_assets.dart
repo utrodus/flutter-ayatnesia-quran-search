52aaab88deb67321f74a752e6e3728e1a8f2af03
utrodus <come.programmer@gmail.com>
2023-02-10 19:05:49 +0700

    fix: assets path name

--- lib/app/res/constant/app_assets.dart
+++ lib/app/res/constant/app_assets.dart
@@ -1,7 +1,7 @@
 class AppAssets {
   const AppAssets._();
-  static const icLogo = "asset/img/ic_logo.png";
-  static const imgEmptySearch = "asset/img/img_empty_search.png";
-  static const imgHandWithQuran = "asset/img/img_hand_with_quran.png";
-  static const imgOnboard = "asset/img/img_onboard.png";
+  static const icLogo = "assets/img/ic_logo.png";
+  static const imgEmptySearch = "assets/img/img_empty_search.png";
+  static const imgHandWithQuran = "assets/img/img_hand_with_quran.png";
+  static const imgOnboard = "assets/img/img_onboard.png";
 }
