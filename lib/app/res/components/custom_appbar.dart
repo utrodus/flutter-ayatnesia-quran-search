fc68ad80973aa5615b345c5d61d39cc49e9373d6
utrodus <come.programmer@gmail.com>
2023-02-11 00:20:12 +0700

    feat: add custom appbar widget

--- lib/app/res/components/custom_appbar.dart
+++ lib/app/res/components/custom_appbar.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:quran_finder/app/res/constant/app_colors.dart';
+import 'package:quran_finder/app/res/theme/app_text_style.dart';
 
-class CustomAppbar extends StatelessWidget {
-  const CustomAppbar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+PreferredSizeWidget customAppBar({
+  required BuildContext context,
+  required String title,
+  required bool isBackButton,
+}) =>
+    PreferredSize(
+      preferredSize: const Size.fromHeight(46),
+      child: AppBar(
+        centerTitle: true,
+        title: Text(
+          title,
+          style: bodyText1Bold(context).copyWith(
+            color: AppColors.onPrimary,
+            fontWeight: semiBold,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: AppColors.primaryGradient,
+            ),
+          ),
+        ),
+      ),
+    );
