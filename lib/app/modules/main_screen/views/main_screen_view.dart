67d0ffab887c2a4c5ae5720b9c1723b02735ef3c
utrodus <come.programmer@gmail.com>
2023-02-11 13:54:35 +0700

    fix: chg little bit ui layout & design

--- lib/app/modules/main_screen/views/main_screen_view.dart
+++ lib/app/modules/main_screen/views/main_screen_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:quran_finder/app/modules/search_verses/views/search_verses_view.dart';
+import 'package:quran_finder/app/res/constant/app_colors.dart';
+import 'package:quran_finder/app/res/theme/app_text_style.dart';
 
 import '../../all_surah/views/all_surah_view.dart';
 import '../controllers/main_screen_controller.dart';
@@ -12,31 +14,99 @@ class MainScreenView extends GetView<MainScreenController> {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => Scaffold(
-        body: IndexedStack(
-          index: controller.tabIndex.value,
-          children: const [
-            SearchVersesView(),
-            AllSurahView(),
-          ],
+      () => SafeArea(
+        child: Scaffold(
+          body: IndexedStack(
+            index: controller.tabIndex.value,
+            children: const [
+              SearchVersesView(),
+              AllSurahView(),
+            ],
+          ),
+          bottomNavigationBar: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: AppColors.background,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.grey.withOpacity(0.1),
+                    blurRadius: 5,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BottomNavItem(
+                    onTap: () => controller.tabIndex.value = 0,
+                    icon: Ionicons.search,
+                    title: "Cari Ayat",
+                    isSelected: controller.tabIndex.value == 0,
+                  ),
+                  BottomNavItem(
+                    onTap: () => controller.tabIndex.value = 1,
+                    icon: Ionicons.book_outline,
+                    title: "Semua Surah",
+                    isSelected: controller.tabIndex.value == 1,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Ionicons.search,
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final bool isSelected;
+  final void Function()? onTap;
+  const BottomNavItem({
+    super.key,
+    required this.onTap,
+    required this.icon,
+    required this.title,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? AppColors.primary.withOpacity(0.1)
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: isSelected ? AppColors.primary : AppColors.grey,
               ),
-              label: "Cari Ayat",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Ionicons.book_outline,
+              const SizedBox(width: 8),
+              Text(
+                title,
+                style: TextStyle(
+                  color: isSelected ? AppColors.primary : AppColors.grey,
+                  fontWeight: isSelected ? semiBold : regular,
+                ),
               ),
-              label: "Semua Surah",
-            ),
-          ],
-          currentIndex: controller.tabIndex.value,
-          onTap: (index) => controller.onChangeTabIndex(index),
+            ],
+          ),
         ),
       ),
     );
--- lib/app/modules/search_verses/views/search_verses_view.dart
+++ lib/app/modules/search_verses/views/search_verses_view.dart
@@ -12,151 +12,147 @@ class SearchVersesView extends GetView<SearchVersesController> {
   const SearchVersesView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 17,
-                  vertical: 14,
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 17,
+              ),
+              width: Get.width,
+              height: Get.height * 0.24,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: AppColors.primaryGradient,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                width: Get.width,
-                height: Get.height * 0.28,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: AppColors.primaryGradient,
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  image: const DecorationImage(
-                      image: AssetImage(AppAssets.imgHandWithQuran),
-                      fit: BoxFit.contain,
-                      alignment: Alignment.bottomRight),
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(12),
-                    bottomRight: Radius.circular(12),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Spacer(),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image(
-                          image: const AssetImage(AppAssets.imgLogo),
-                          width: Get.width * 0.08,
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          "Qur'an Finder",
-                          style: h5Bold(context).copyWith(
-                            color: AppColors.onPrimary,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "Solusi untuk Menemukan Ayat\nAl Qur’an sesuai kebutuhanmu",
-                      style: h5Bold(context).copyWith(
-                        color: AppColors.onPrimary,
-                        fontWeight: medium,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    Obx(() => TextFormField(
-                          controller: controller.searchTextFieldController,
-                          focusNode: controller.searchFocusNode,
-                          decoration: InputDecoration(
-                            prefixIcon: const Icon(Ionicons.search),
-                            hintText: "Masukkan kata kunci",
-                            hintStyle: bodyText1Regular(context).copyWith(
-                              color: AppColors.secondary.withOpacity(0.5),
-                            ),
-                            suffixIcon: controller.searchQuery.isEmpty
-                                ? null
-                                : GestureDetector(
-                                    onTap: () =>
-                                        controller.onTapClearSearchTextField(),
-                                    child: Icon(
-                                      Icons.close_rounded,
-                                      size: 20,
-                                      color:
-                                          Theme.of(context).colorScheme.error,
-                                    ),
-                                  ),
-                          ),
-                          textInputAction: TextInputAction.search,
-                          style: bodyText1Bold(context),
-                          onChanged: (value) =>
-                              controller.onChangedSearchTextField(value),
-                          onFieldSubmitted: (_) =>
-                              controller.onFieldSubmittedSearchTextField(),
-                        )),
-                    const Spacer(),
-                  ],
+                image: const DecorationImage(
+                    image: AssetImage(AppAssets.imgHandWithQuran),
+                    fit: BoxFit.contain,
+                    alignment: Alignment.bottomRight),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
                 ),
               ),
-              Obx(() {
-                switch (controller.appState.value) {
-                  case AppState.initial:
-                    return Padding(
-                      padding: EdgeInsets.only(top: Get.height * 0.13),
-                      child: Center(
-                          child: Column(
-                        children: [
-                          Image(
-                            image: const AssetImage(
-                              AppAssets.imgInitialState,
-                            ),
-                            width: Get.width * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: const AssetImage(AppAssets.imgLogo),
+                        width: Get.width * 0.08,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Qur'an Finder",
+                        style: h5Bold(context).copyWith(
+                          color: AppColors.onPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Solusi untuk Menemukan Ayat\nAl Qur’an sesuai kebutuhanmu",
+                    style: h5Bold(context).copyWith(
+                      color: AppColors.onPrimary,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Obx(() => TextFormField(
+                        controller: controller.searchTextFieldController,
+                        focusNode: controller.searchFocusNode,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Ionicons.search),
+                          hintText: "Masukkan kata kunci",
+                          hintStyle: bodyText1Regular(context).copyWith(
+                            color: AppColors.secondary.withOpacity(0.5),
                           ),
-                          const SizedBox(height: 21),
-                          Text(
-                            "Cari Ayat Al Qur’an",
-                            style: h5Bold(context).copyWith(
-                              fontWeight: semiBold,
-                            ),
-                          ),
-                          const SizedBox(height: 7),
-                          const Text(
-                            "Masukkan kata kunci untuk\nmencari ayat Al Qur’an",
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
+                          suffixIcon: controller.searchQuery.isEmpty
+                              ? null
+                              : GestureDetector(
+                                  onTap: () =>
+                                      controller.onTapClearSearchTextField(),
+                                  child: Icon(
+                                    Icons.close_rounded,
+                                    size: 20,
+                                    color: Theme.of(context).colorScheme.error,
+                                  ),
+                                ),
+                        ),
+                        textInputAction: TextInputAction.search,
+                        style: bodyText1Bold(context),
+                        onChanged: (value) =>
+                            controller.onChangedSearchTextField(value),
+                        onFieldSubmitted: (_) =>
+                            controller.onFieldSubmittedSearchTextField(),
                       )),
-                    );
-                  case AppState.loading:
-                    return Container();
-                  case AppState.success:
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 25.0,
-                        vertical: 16.0,
-                      ),
-                      child: Column(
-                        children: const [
-                          Text("Hasil Pencarian Ayat: 0 Ayat"),
-                        ],
-                      ),
-                    );
-                  case AppState.error:
-                    return Container();
-                  default:
-                    return Container();
-                }
-              })
-            ],
-          ),
+                  const Spacer(),
+                ],
+              ),
+            ),
+            Obx(() {
+              switch (controller.appState.value) {
+                case AppState.initial:
+                  return Padding(
+                    padding: EdgeInsets.only(top: Get.height * 0.13),
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Image(
+                          image: const AssetImage(
+                            AppAssets.imgInitialState,
+                          ),
+                          width: Get.width * 0.5,
+                        ),
+                        const SizedBox(height: 21),
+                        Text(
+                          "Cari Ayat Al Qur’an",
+                          style: h5Bold(context).copyWith(
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                        const SizedBox(height: 7),
+                        const Text(
+                          "Masukkan kata kunci untuk\nmencari ayat Al Qur’an",
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    )),
+                  );
+                case AppState.loading:
+                  return Container();
+                case AppState.success:
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 16.0,
+                    ),
+                    child: Column(
+                      children: const [
+                        Text("Hasil Pencarian Ayat: 0 Ayat"),
+                      ],
+                    ),
+                  );
+                case AppState.error:
+                  return Container();
+                default:
+                  return Container();
+              }
+            })
+          ],
         ),
       ),
     );
