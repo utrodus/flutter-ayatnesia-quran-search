c810631dbf0e97c3a027d011ff0e03008b8a28e7
utrodus <contact.utrodus@gmail.com>
2024-09-19 22:27:07 +0700

    feat: change baseUrl to prodution and add docs related to usage base url

--- lib/src/core/network/api_config.dart
+++ lib/src/core/network/api_config.dart
@@ -1,10 +1,17 @@
 class ApiConfig {
   ApiConfig._();
 
-  /// for android emulator use http://10.0.2.2:8000
+  /// Base URL configuration for the Flutter Ayatnesia project:
+  /// If running API locally (https://github.com/utrodus/fastapi-ayatnesia), use the following base URL:
+  /// local development
+  /// - **Android Emulator**: `http://10.0.2.2:8000`
+  /// - **iOS Emulator or Real Device**: `http://localhost:8000`
   ///
-  /// for ios emulator or real device use http://localhost:8000
-  static const String baseUrl = "http://10.0.2.2:8000/api";
+  /// For production or live API access, use:
+  /// - **Live API**: `https://api.ayatnesia.unublitar.ac.id/api`
+  ///   This is the base URL for accessing the live version of the API in a production environment.
+
+  static const String baseUrl = "https://api.ayatnesia.unublitar.ac.id/api";
 
   static const Duration receiveTimeout = Duration(seconds: 500);
   static const Duration connectionTimeout = Duration(seconds: 500);
